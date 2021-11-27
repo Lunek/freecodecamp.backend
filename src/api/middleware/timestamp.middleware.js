f2a6e7b65306185582ca9c1af65e1107f906359f
Alexander Belakovskyi <Alex-Borisov992@list.ru>
2021-11-27 15:09:55 +0200

    Removed moment() from project

--- src/api/middleware/timestamp.middleware.js
+++ src/api/middleware/timestamp.middleware.js
@@ -1,22 +1,25 @@
-const moment = require('moment');
-
 const validateAndCastDateToMoment = (req, res, next) => {
   const sendDate = req.params.date;
-  // try cast the date to moment object
-  const dateInNormalFormat = moment(sendDate, "YYYY-MM-DD");
-  const dateInUnixFormat = moment.unix(sendDate);
-  // validate it and pass to handler parsedDate
-  if (dateInNormalFormat.isValid()) {
-    req.parsedDate = dateInNormalFormat;
-    next();
-  } else if (dateInUnixFormat.isValid()) {
-    req.parsedDate = dateInUnixFormat;
-    next();
+
+  // detect if received date is unix timestamp
+  const isReceivedDateUnix = !isNaN(Number(sendDate));
+
+  if (isReceivedDateUnix) {
+    // if received unix
+    req.parsedDate = new Date(Number(sendDate));
+  } else if (isDate(sendDate)) {
+    // if received is string and can be parsed to date
+    req.parsedDate = new Date(sendDate);
   } else {
     res.send({ error: "Invalid Date" });
   }
-}
+  next();
+};
+
+const isDate = (date) => {
+  return new Date(date) !== "Invalid Date" && !isNaN(new Date(date));
+};
 
 module.exports = {
-  validateAndCastDateToMoment
+  validateAndCastDateToMoment,
 };
--- src/api/routes/timestamp.microservice.js
+++ src/api/routes/timestamp.microservice.js
@@ -1,27 +1,26 @@
 const router = require('express').Router();
-const moment = require('moment');
 const {
   validateAndCastDateToMoment
 } = require("../middleware/timestamp.middleware.js");
 
 router.get("/:date", validateAndCastDateToMoment, (req, res) => {
   // We can get parsed date from middleware
-  const parsedDate = req.parsedDate;
+  const parsedDate = new Date(req.parsedDate);
 
   const response = {
-    unix: parsedDate.unix(),
-    utc: parsedDate.toDate().toUTCString(),
+    unix: parsedDate.getTime(),
+    utc: parsedDate.toUTCString(),
   };
 
   res.send(response);
 });
 
 router.get("/", (req, res) => {
-  const currentDate = moment();
+  const currentDate = new Date();
 
   const response = {
-    unix: currentDate.unix(),
-    utc: parsedDate.toDate().toUTCString(),
+    unix: currentDate.getTime(),
+    utc: currentDate.toUTCString(),
   };
 
   res.send(response);
