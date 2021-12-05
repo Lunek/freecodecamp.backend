2901f847b2c89d4227a5ffe0dc6e33e19fe800e8
Alexander Belakovskyi <Alex-Borisov992@list.ru>
2021-12-05 13:07:35 +0200

    Added endpoint for uploading files

--- src/api/routes/file-metadata.microservice.js
+++ src/api/routes/file-metadata.microservice.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-router.post("/api/fileanalyse", (req, res) => {
-  console.log('FILE RECEIVED', req.body);
-  res.status(500).send("An error occured");
+router.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const file = req.file;
+
+  if (file) {
+    res.status(200).send({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size,
+    });
+  } else {
+    res.status(500).send("An error occured");
+  }
 });
 
 module.exports = router;
